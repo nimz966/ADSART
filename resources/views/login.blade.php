123760a960c889aacbe961bc7717c855662ec1f4
nimz966 <nimnayasasni@gmail.com>
2021-06-27 15:14:48 +0530

    update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,7 +17,7 @@
     <style>
     /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
     body {
-        background: url('img/4to1.jpg') no-repeat center center fixed;
+        background: url('img/bc.png') no-repeat center center fixed;
         -webkit-background-size: cover;
         -moz-background-size: cover;
         background-size: cover;
@@ -26,9 +26,14 @@
 
     .main-section {
         margin: 0 auto;
-        margin-top: 130px;
+        margin-top: 40px;
         padding: 0;
+    }
 
+    .modal-dialog {
+        float: right;
+        width: 1000px;
+        padding-right: 50px;
     }
 
     .modal-content {
@@ -39,72 +44,108 @@
     }
 
     .user-img img {
-        height: 120px;
-        width: 120px;
+        height: 100px;
+        width: 100px;
     }
 
     .user-img {
         margin-top: -40px;
-        margin-bottom: 45px;
+        margin-bottom: 35px;
+        text-align: center;
     }
 
     .form-group {
         padding: 10px;
     }
-    </style>
 
-    <style>
     body {
         font-family: 'Nunito', sans-serif;
+    }
+
+    #phone {
+        text-align: left !important;
+    }
+
+    #navitem {
+        padding-right: 20px;
     }
     </style>
 </head>
 
 <body class="antialiased">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="img/logo.png" height="35"></a>
-            <a class="navbar-brand" href="#">ADS ART</a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
+            <div class="col">
+                <a class="navbar-brand" href="#"><img src="img/logo.png" height="40"> </a>
+                <a class="navbar-brand" href="#"> ADS ART Event Management System</a>
             </div>
         </div>
-    </nav>
-    <div class="modal-dialog text-center">
-        <div class="col-sm-9 main-section">
-            <div class="modal-content">
-                <div class="col-12 user-img">
-                    <img src="img/download.png">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <form>
+                <ul class="navbar-nav">
+                    <li class="nav-item" id="navitem">
+                        <input type="email" class="form-control h-75" placeholder="Enter email">
+                    </li>
+                    <li class="nav-item" id="navitem">
+                        <input type="password" class="form-control h-75" placeholder="Enter password">
+                    </li>
+                    <li class="nav-item" id="navitem">
+                        <button type="submit" class="btn btn-primary h-75 mb-2">Login</button>
+                    </li>
+                </ul>
+            </form>
+        </div>
 
-                </div>
+    </nav>
+    <div class="modal-dialog">
+        <div class="col main-section">
+            <div class="modal-content">
+
                 <div class="col-12 form-input">
                     <form>
                         <div class="form-group">
+                            <input type="text" class="form-control" placeholder="User name">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Enter address">
+                        </div>
+                        <div class="form-group">
+                            <input type="tel" id="phone" class="form-control" placeholder="Enter contact number"
+                                pattern="[0-9]{3} [0-9]{7}">
+                            <small>Format: 011 8645678</small>
+                        </div>
+                        <div class="form-group">
                             <input type="email" class="form-control" placeholder="Enter email">
                         </div>
-
+                        <div>
+                            <div class="row m-1">
+                                <lable>User Type</lable>
+                            </div>
+                            <div class="row">
+                                <div class="col text-center">
+                                    <input type="radio" id="customRadio1" name="customRadio"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label mr-4" for="customRadio1">CEO</label>
+                                </div>
+                                <div class="col text-center">
+                                    <input type="radio" id="customRadio2" name="customRadio"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio2">Manager</label>
+                                </div>
+                                <div class="col text-center">
+                                    <input type="radio" id="customRadio1" name="customRadio"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="customRadio3">Employee</label>
+                                </div>
+                            </div>
+                        </div>
                         <div class="form-group">
                             <input type="password" class="form-control" placeholder="Enter password">
                         </div>
-                        <button type="submit" class="btn btn-success">Login</button>
+                        <div class="col text-center">
+                            <button type="submit" class="btn btn-primary">Register</button>
+                        </div>
+
                     </form>
                 </div>
             </div>
